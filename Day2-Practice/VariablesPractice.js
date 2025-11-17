9b95afc239c261e67478bb49879a068b208119a3
ReddyHema01 <hema.reddy01@yahoo.co.uk>
2025-11-17 21:44:26 +0000

    Variables Practice

---
+++ Day2-Practice/VariablesPractice.js
@@ -0,0 +1,33 @@
+//variables practice
+
+var name = "Hema";
+console.log(name); // Output: Hema
+var name = "Reddy";
+console.log(name); // Output: Reddy
+name = "KK";
+console.log(name); // Output: KK
+// AGE
+let age = 25;
+console.log(age); // Output: 25
+age =30;
+console.log(age); // Output: 30
+//COUNTRY
+const country = "UK";
+console.log(country); // Output: UK
+
+//login information
+let username = "hemareddy";
+const password = "hema@123";
+console.log("Username:", username);
+console.log("password:",password);
+
+//Change username
+username = "hemar";
+age = 30;
+console.log("Updated Username:", username);
+console.log("Updated Age:", age);
+
+
+
+
+
