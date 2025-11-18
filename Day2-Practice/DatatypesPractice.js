4109509d8d7c736bf43879fa49de77083d59904c
ReddyHema01 <hema.reddy01@yahoo.co.uk>
2025-11-18 12:01:31 +0000

    DataType Practice

---
+++ Day2-Practice/DatatypesPractice.js
@@ -0,0 +1,73 @@
+//Datatype practice 
+//String : city Name
+let city = "Leicester";
+console.log(typeof city); // Output: string
+console.log(city);
+
+//Number data type
+let population = 100000;
+console.log(typeof population); // Output: number
+console.log(population);
+
+//Testing Boolean 
+let iscapital = true;
+console.log(typeof iscapital); // Output: boolean
+console.log(iscapital);
+
+//undefined data type
+let candidate;
+console.log(typeof candidate); // Output: undefined
+console.log(candidate);
+
+//output undefined value
+let candidatename = "Hema";
+candidatename = undefined;
+console.log(typeof candidatename); state = null; // Output: object (this is a known quirk in JavaScript)
+console.log(candidatename);state = null;
+console.log(typeof state);state = null;
+console.log(state);state 
+
+//Testing Null values
+let citycode = null;
+console.log(typeof citycode); // Output: object (this is a known quirk in JavaScript)
+console.log(citycode);
+
+//Symbol data type
+let uniqueid = Symbol("NI)");
+console.log(typeof uniqueid); // Output: symbol
+console.log(uniqueid);
+
+//Big integer data type
+let bignumber = BigInt(100000000000000);
+console.log(typeof bignumber); // Output: bigint
+console.log(bignumber);
+
+//Non-Primitive Data Types 
+//Testing Object
+let person = {
+    firstname: "Hema",
+    lastname: "Reddy",
+    age: 30
+};
+console.log(typeof person); // Output: object
+console.log(person);
+
+//Testing Array data type
+let shapes = ["Circle", "Rectangle", "Triangle", "Square"];
+console.log(typeof shapes); // Output: object
+console.log(shapes);
+
+//Testing functional data type
+function gratitude() {
+    return "happy";
+}
+console.log(typeof gratitude); // Output: function
+console.log(gratitude());
+
+
+
+
+
+
+
+
